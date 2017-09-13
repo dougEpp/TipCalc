cc33491cf580ec06dc0e6d52b2535d2d9fb42718
Doug Epp <Depp1715@conestogac.on.ca>
2017-09-13 13:47:13 -0400

    Committed project

---
+++ app/src/main/java/ca/on/conestogac/depp1715/tipcalc/TipCalculator.java
@@ -0,0 +1,100 @@
+package ca.on.conestogac.depp1715.tipcalc;
+
+import java.text.NumberFormat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
+import android.app.Activity;
+
+public class TipCalculator extends Activity implements OnEditorActionListener, OnClickListener {
+    private EditText billAmountEditText;
+    private TextView percentTextView;
+    private Button   percentUpButton;
+    private Button   percentDownButton;
+    private TextView tipTextView;
+    private TextView totalTextView;
+    private CheckBox roundUp;
+
+    // tip percent variable - default to 20%
+    private float tipPercent = .20f;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tip_calculator);
+
+        billAmountEditText = (EditText) findViewById(R.id.billAmountEditText);
+        percentTextView = (TextView) findViewById(R.id.percentTextView);
+        percentUpButton = (Button) findViewById(R.id.percentUpButton);
+        percentDownButton = (Button) findViewById(R.id.percentDownButton);
+        tipTextView = (TextView) findViewById(R.id.tipTextView);
+        totalTextView = (TextView) findViewById(R.id.totalTextView);
+        roundUp = (CheckBox) findViewById(R.id.checkbox_roundup);
+
+        billAmountEditText.setOnEditorActionListener(this);
+        percentUpButton.setOnClickListener(this);
+        percentDownButton.setOnClickListener(this);
+        roundUp.setOnClickListener(this);
+
+        calculateAndDisplay();
+    }
+
+    public void calculateAndDisplay() {
+        // get the bill amount
+        String billAmountString = billAmountEditText.getText().toString();
+        float billAmount;
+        if (billAmountString.equals("")) {
+            billAmount = 0;
+        }
+        else {
+            billAmount = Float.parseFloat(billAmountString);
+        }
+
+        // calculate tip and total
+        float tipAmount = billAmount * tipPercent;
+        float totalAmount = billAmount + tipAmount;
+
+        if (roundUp.isChecked()){
+            totalAmount = (float)Math.ceil(totalAmount);
+            tipAmount = totalAmount - billAmount;
+        }
+
+        // display the results with formatting
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        tipTextView.setText(currency.format(tipAmount));
+        totalTextView.setText(currency.format(totalAmount));
+
+        NumberFormat percent = NumberFormat.getPercentInstance();
+        percentTextView.setText(percent.format(tipPercent));
+    }
+
+    @Override
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        calculateAndDisplay();
+        return false;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.percentDownButton:
+                tipPercent = tipPercent - .01f;
+                calculateAndDisplay();
+                break;
+            case R.id.percentUpButton:
+                tipPercent = tipPercent + .01f;
+                calculateAndDisplay();
+                break;
+            default:
+                calculateAndDisplay();
+                break;
+        }
+    }
+}
